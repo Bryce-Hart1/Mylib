b62807e0cd1739e831d50ffe2594aea63826d200
Bryce Hart <bryce.hart23@gmail.com>
2026-03-05 11:35:21 -0500

    boilerplate finished

---
+++ tsStack.hpp
@@ -0,0 +1,26 @@
+#include "tsVector.hpp"
+
+template <typename T>
+using vec = threadsafe::vec<T>;
+
+
+namespace threadsafe{
+
+    template <typename T>
+    class queue{
+        friend class vec<T>;
+        private:
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+}
