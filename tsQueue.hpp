fa3486ccf5ad260a40979ce43dd42fe3109bde6a
Bryce Hart <bryce.hart23@gmail.com>
2026-03-18 14:39:13 -0400

    added more to queue

--- tsQueue.hpp
+++ tsQueue.hpp
@@ -1,49 +1,47 @@
-#include "tsVector.hpp"
-
-
 #include <shared_mutex>
-
+#include <memory>
 
 
 /**
  * @attention a threadsafe queue
  * Requires import of threadsafe vector
  */
-template <typename T>
-using vec = threadsafe::vec<T>;
-
-
 namespace threadsafe{
 
         template <typename T> class queue{
-        friend class vec<T>;
+        friend class threadsafe::vec<T>;
         
         private:
             vec<T> _queue;
-            std::atomic<std::size_t> _front;
-            std::atomic<std::size_t> _back;
+            std::atomic<std::size_t> frontPtr;
 
         void clear(){
-            this->_queue.clear();
+            _queue.clear();
+        }
+
+        void clear(bool RESET_CAPACITY){
+            _queue.clear(RESET_CAPACITY);
+        }
+
+        bool isEmpty(){
+            return _queue.isEmpty();
         }
 
         T front(){
-            return vec.at(_front);            
+            return _queue.at(frontPtr);            
         }
 
         void pop(){
-            _front.fetch_add(1);
+            frontPtr.fetch_add(1);
         }
 
         void push(T val){
-            _queue.pushBack(val)
+            _queue.pushBack(val);
         }
 
-
+        std::size_t size(){
+            return _queue.size();
+        }
     };
-
-
-
-
 
 }
