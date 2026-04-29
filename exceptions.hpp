cfe41254e97ea3fc5d2e8bf48d3439d744d224be
Bryce Hart <bryce.hart23@gmail.com>
2026-04-29 16:18:10 -0400

    removing unfoldered files

--- exceptions.hpp
+++
@@ -1,9 +0,0 @@
-#include <exception>
-#include <string>
-
-class outOfBoundsException : public std::exception{
-    std::string errorMsg;
-    public:
-    outOfBoundsException(std::string message) : errorMsg(std::move(message)){}
-    const char* what() const noexcept override {return errorMsg.c_str(); }
-};
